5e10dc8b8a7777f4e385afb4a8555957f7fe37d9
rmslick <rmslick@buffalo.edu>
2022-01-27 05:56:07 -0500

    Assignment 2

--- assignments/Slick/2/server.js
+++ assignments/Slick/2/server.js
@@ -1,18 +1,14 @@
 const express = require('express');
+var favicon = require('serve-favicon')
 const app = express();
 const path = require('path');
-var cookieParser = require('cookie-parser')
+
 
 const router = express.Router();
 // Form here:https://codeforgeek.com/render-html-file-expressjs/
 
 router.get('/',function(req,res){
-  //res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-    // Cookies that have not been signed
-    console.log('Cookies: ', req.cookies)
-
-    // Cookies that have been signed
+    console.log('Hey there, rockstar. Looking for one of these?')
 });
 
 router.get('/blink182',function(req,res){
@@ -22,17 +18,18 @@ router.get('/blink182',function(req,res){
 router.get('/knoxville',function(req,res){
     res.sendFile(path.join(__dirname+'/sites/knoxville.html'));
 });
-router.get('/sexpistols',function(req,res){
+router.get('/bigdandthekidstable',function(req,res){
     res.sendFile(__dirname+'/sites/bigdandthekidstable.html');
 });
 router.get('/harrypotter',function(req,res){
     res.sendFile(path.join(__dirname+'/sites/harrypotter.html'));
 });
 //add the router
+app.use(favicon(path.join(__dirname,'/favicon/favicon.ico')))
 app.use(express.static('public'))
 app.use('/', router);
-app.use(cookieParser())
-
+var publicDir = require('path').join(__dirname,'/sites');
+app.use(express.static(publicDir));
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
