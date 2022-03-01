d554cd3e8b3854ce8ddc37f9552a0abb25962f83
raphabiog <eabio001@odu.edu>
2022-03-01 04:08:15 -0500

    A3 submission update4

--- assignments/Abiog/3/GetMore.sh
+++ assignments/Abiog/3/GetMore.sh
@@ -15,9 +15,9 @@ for FILE in HttpResponses/*; do
         grep -i -o "HttpOnly" $FILE | wc -l >> "$httponly"
         grep -i -o "Secure" $FILE | wc -l >> "$secure"
         grep -i -o "SameSite" $FILE | wc -l >> "$samesite"
-        grep -i -o "strict" $FILE | wc -l >> "$strict"
-        grep -i -o "lax" $FILE | wc -l >> "$lax"
-        grep -i -o "none" $FILE | wc -l >> "$none"
+        grep -i -o "samesite=strict" $FILE | wc -l >> "$strict"
+        grep -i -o "samesite=lax" $FILE | wc -l >> "$lax"
+        grep -i -o "samesite=none" $FILE | wc -l >> "$none"
         grep -i -o "path=" $FILE | wc -l >> "$path"
         grep -i -o "path=/[a-zA-Z0-9]" $FILE | wc -l >> "$pathset"
 done
