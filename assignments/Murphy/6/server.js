678efe4a59ab2ef14a78e75b9a3d878e3df8b308
jmurp030 <jmurp030@odu.edu>
2022-04-17 22:32:09 -0400

    server now recognizes if user has visted before

--- assignments/Murphy/6/server.js
+++ assignments/Murphy/6/server.js
@@ -7,12 +7,17 @@ const app1 = express()
 const md5 = require('md5')
 var csv = 'test.csv'
 
+// Fingerprinting based on Res.Headers
+// Takes in three header parameters, concatenates them together, hashes
+// the combined variable, and returns it
 function fingerPrinting(data_1, data_2, data_3){
     data_final = data_1 + data_2 + data_3
     data_hash = md5(data_final)
     return(data_hash)
 }
 
+// Prints out user-agent, accept, and accept-language headers, as well as 
+// the hash value created in the fingerPrinting() function.
 function printLog(data_1, data_2, data_3, data_4){
     console.log('User-Agent: ' + data_1)
     console.log('Accept: ' + data_2)
@@ -20,31 +25,45 @@ function printLog(data_1, data_2, data_3, data_4){
     console.log('Hash: ' + data_4 + '\n')
 }
 
-// Write data to file
-const storeData = (data_1, data_2, data_3, data_4, path) => {
+// Checks if file 'test.csv' exists.
+// If it does not, it creates it.
+function checkForFile(path){
+    if (!fs.existsSync(csv))
+        fs.writeFileSync(path, 'hash', json)
+}
+// Write data to file only if the value is unique.
+var storeData = (data_1, path, array) => {
     try {
         // Store data parameters into string variable
+        /*
         var str = JSON.stringify(data_1) + "," + 
                   JSON.stringify(data_2) + "," + 
                   JSON.stringify(data_3) + "," + 
                   JSON.stringify(data_4) + 
                   '\n'
+        */
+        var hash = JSON.stringify(data_1) 
         // Remove double quotes and backslash added in writing process
-        var res = str
+        var res = hash
             res = res.toString()
             res = res.replace(/\\/g, '')
             res = res.replaceAll('""', '"')
-        // if statement to check if csv file exists. 
-        // if it does not, create it and save string to file.
-        if (fs.existsSync(csv))
-            fs.appendFile("test.csv", (res), function(err){
+            res = res.replaceAll('"', '')
+
+        if (!array.includes(data_1)) {
+            fs.appendFile(path, '\n' + (res), function(err){
                 if (err) throw err
             })
-        else
-            fs.writeFileSync(path, 'hash, user-agent, accept, accept-langauge\n' + res, json)
+        }  
     } catch(err) {
         console.error(err)
     }
+}
+
+// Reads 'test.csv' into variable and returns it
+function readData(){
+    var data = fs.readFileSync('test.csv', 'utf8').toString().split("\n")
+    return(data)
 }
 
 //Static Files
@@ -70,10 +89,13 @@ app1.get('/', (req, res) => {
     var ac = JSON.stringify(req.headers['accept'])
     var al = JSON.stringify(req.headers['accept-language'])
 
-   storeData(data_hash, ua, ac, al, csv)
+    checkForFile(csv)
+    hashArray = readData()
+    console.log(hashArray)
 
     var hash = data_hash
-    res.render('homepage', {hash:hash})
+    res.render('homepage', {data: {hash:hash, hashArray:hashArray}})
+    storeData(data_hash, csv, hashArray)
 })
 
 app1.get('/rage-against-the-machine', (req, res) => {
